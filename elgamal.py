1178b4932936f3eb7944460018e98b745c7d0e4e
Ali Sheikh <ali.sheikh@flexport.com>
2021-06-01 23:07:58 -0400

    new

--- elgamal.py
+++ elgamal.py
@@ -22,24 +22,23 @@ def encrypt(pk,m):
     c2 = (pow(pk, r, p) * (m % p)) %  p
     return [c1,c2]
 
-# def decrypt(sk,c):
-#     c0 = c[0]
-#     #use array index
-#     c1 = c[1]
-#     #use array index
-
-#     funcC0 = pow(c0, sk, p)
-
-#     #Modular Inverse
-#     funcC1 = c1 * pow(funcC0, -1, p)
-#     #Define M
-#     m = pow(funcC1, 1, p)
-#     return m
-
 def decrypt(sk,c):
-    c1 = c[0]
-    c2 = c[1]
-    temp = pow(c1, sk, p)
-    temp2 = c2 * pow(temp, -1, p)
-    m = pow(temp2, 1, p)
+    c0 = c[0]
+    #use array index
+    c1 = c[1]
+    #use array index
+    funcC0 = pow(c0, sk, p)
+
+    #Modular Inverse
+    funcC1 = c1 * pow(funcC0, -1, p)
+    #Define M
+    m = pow(funcC1, 1, p)
     return m
+
+# def decrypt(sk,c):
+#     c1 = c[0]
+#     c2 = c[1]
+#     temp = pow(c1, sk, p)
+#     temp2 = c2 * pow(temp, -1, p)
+#     m = pow(temp2, 1, p)
+#     return m
